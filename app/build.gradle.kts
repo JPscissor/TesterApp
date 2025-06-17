dc155eb07bf4abc206108f5ea11934107748721b
Weyne <Weyne@WEYNE>
2025-06-17 18:20:10 +0300

    Up version to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.4"
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
